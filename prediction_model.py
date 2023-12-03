ffe0ee3b1e4a625614f4ce88e05ed3862be937db
Firuz Juraev <62535926+Firuz-JuraevML@users.noreply.github.com>
2023-12-03 20:07:54 +0500

    Update prediction_model.py

--- prediction_model.py
+++ prediction_model.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd 
 import numpy as np  
 import pickle 
-from lightgbm import LaBMClassifier
+from lightgbm import LGBMClassifier
 from lightgbm import LGBMRegressor 
 import streamlit as st 
 from sklearn.preprocessing import MinMaxScaler  
